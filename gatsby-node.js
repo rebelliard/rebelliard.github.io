ea2d79ff4c47d737f692661a5d65ff12f49accd6
Rafael Belliard <belliard@gmail.com>
2018-01-26 01:30:22 -0400

    Handle post redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 
 exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators
+  const { createPage, createRedirect } = boundActionCreators
 
   return new Promise((resolve, reject) => {
     const blogPost = path.resolve('./src/templates/blog-post.js')
@@ -23,6 +23,7 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
                     teaser
                     description
                     tags
+                    redirectFrom
                   }
                 }
               }
@@ -41,16 +42,45 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
         const createPost = (post, index) => {
           const previous = index === posts.length - 1 ? false : posts[index + 1].node;
           const next = index === 0 ? false : posts[index - 1].node;
+          const path = post.node.fields.slug
 
           createPage({
-            path: post.node.fields.slug,
+            path: path,
             component: blogPost,
             context: {
-              slug: post.node.fields.slug,
+              slug: path,
               previous,
               next,
             },
           })
+
+          const {redirectFrom} = post.node.frontmatter
+
+          /**
+           * Need to handle trailing-slashed and non-trailing-slashed
+           * fromPaths separately, Gatsby serves page components
+           * at either version by default, but we need to explicitly redirect
+           * both versions independently, more on page components:
+           * @see https://gatsbyjs.org/docs/building-with-components/
+           * @see https://gatsbyjs.org/docs/creating-and-modifying-pages/#removing-trailing-slashes
+           */
+          if (Array.isArray(redirectFrom) && redirectFrom.length) {
+            redirectFrom.forEach(redirect => {
+              const paths = [
+                redirect,
+                redirect.endsWith('/') ? redirect.slice(0, -1) : `${redirect}/`
+              ]
+
+              paths.forEach(fromPath => {
+                createRedirect({
+                  fromPath,
+                  toPath: path,
+                  isPermanent: true,
+                  redirectInBrowser: true,
+                })
+              })
+            })
+          }
         }
 
         _.each(posts, createPost)
