bb74f09b3559c93258ea318772cc48722ef6e986
Rafael Belliard <belliard@gmail.com>
2018-04-22 09:53:20 -0400

    Properly set the title

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,8 +3,9 @@ import Helmet from 'react-helmet'
 
 const Head = props => {
   return (
-    <Helmet {...props}>
+    <Helmet>
       <html lang="en"/>
+      <title>{props.title}</title>
       <meta name="description" content={props.description}/>
       <meta name="theme-color" content="#22297c"/>
     </Helmet>
