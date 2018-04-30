6adf8174fbb5185101a4d4f50ee50507c99b99ec
Rafael Belliard <belliard@gmail.com>
2018-04-30 12:36:33 -0400

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-a84a9fc8407154c0ecba.js","d804936e67d94094c67e3d7a418dde63"],["/commons-b746b4f6ee97c0a7f7ce.js","443b5b48b81c646bfa5ac2e0c60b85de"],["/index.html","dc9ed37c074c3cc123fdb0f8ef7bb83b"],["/offline-plugin-app-shell-fallback/index.html","b65be5562b38dedd60c67d1b64641bdf"]];
+var precacheConfig = [["/app-a84a9fc8407154c0ecba.js","d804936e67d94094c67e3d7a418dde63"],["/commons-b746b4f6ee97c0a7f7ce.js","443b5b48b81c646bfa5ac2e0c60b85de"],["/index.html","22b381a75ada093d9391b819892c5743"],["/offline-plugin-app-shell-fallback/index.html","fe756454a780fc7fd23b34ba849be7b1"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
